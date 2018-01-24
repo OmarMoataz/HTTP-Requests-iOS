9924ffb481222ca35941288fb51af5181a2ff02b
Omar Moataz <omarmoataz@outlook.com>
2018-01-24 18:03:01 +0200

    Initial Commit

---
+++ HTTPRequests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTTPRequests
+//
+//  Created by Omar Moataz Abdel-Wahed Attia on 1/24/18.
+//  Copyright © 2018 Omar Moataz Abdel-Wahed Attia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
